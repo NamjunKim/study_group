66648cd9ee8027eb45edaea0f0b1be64e16db155
namjunkim <namjun88@gmail.com>
2021-03-07 10:58:21 +0900

    hello world

---
+++ skeleton/src/test/java/com/skeleton/project/SkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skeleton.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
